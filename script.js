754875079a622e9bb1e88c5448a114e2839b2f62
Dryelle Prietsch <dryelle_andressa@hotmail.com>
2024-10-07 20:35:10 -0300

    function arrayMaxMin
    Acrescentado a function arrayMaxMin

--- script.js
+++ script.js
@@ -11,5 +11,17 @@ function isPalindrome(str){
 }
 
 function arrayMaxMin(arr){
-    
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return [min, max];
 }
