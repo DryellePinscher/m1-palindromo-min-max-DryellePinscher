332f34b5b68bd5adcb812e35f973227c234dd8d2
Dryelle Prietsch <dryelle_andressa@hotmail.com>
2024-10-07 20:31:23 -0300

    Update script.js
    Retirado o comentário e acrescentado a function do palíndromo

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const length = cleanStr.length;
+
+    for (let i = 0; i < length / 2; i++) {
+        if (cleanStr[i] !== cleanStr[length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    
 }
